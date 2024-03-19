54274611679818ba7d43e659fdd59887fd81dd2a
Bibaev Ruslan <bibaevsurlan@gmail.com>
2024-03-19 22:58:07 +0400

    init: initial commit

---
+++ src/main/java/com/github/valence/ValenceApplication.java
@@ -0,0 +1,13 @@
+package com.github.valence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/valence/ValenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.valence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
